db0f637cb4d203ea08f41057008df050df2e1ae5
Beulakv <beulavarghese2016@gmail.com>
2020-07-04 15:05:10 +0530

    107

--- src/app.js
+++ src/app.js
@@ -25,7 +25,14 @@ const PLAYERS = [
 const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
-    // Type your code here
+    for (i = 0; i < players.length; i++) {
+        detailedPlayers[i] = {
+            name: players[i],
+            image: "images/super-" + (i + 1) + ".png",
+            strength: getRandomStrength(),
+            type: (i % 2 == 0) ? "hero" : "villain"
+        };
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +41,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random() * 100);
 }
 
 const buildPlayers = (players, type) => {
@@ -41,7 +49,16 @@ const buildPlayers = (players, type) => {
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
-    // Type your code here
+    for (i = 0; i < players.length; i++) {
+        player = `
+            <div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+            </div> `;
+        if (players[i].type == type)
+            fragment += player;
+    }
 
     return fragment;
 }
